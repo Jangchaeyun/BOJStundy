78f73acc33a9dfe50e4823e1fa433f12a60d9f25
Jangchaeyun <zzangsally@naver.com>
2023-09-19 11:06:24 +0900

    0919

---
+++ 브론즈2PY/0919/B4613.py
@@ -0,0 +1,16 @@
+answer = list()
+while True:
+    s = list(map(str, input()))
+    if s[0] == '#':
+        for k in range(len(answer)):
+            print(answer[k])
+        break
+    else:
+        total = 0
+        for i in range(len(s)):
+            if s[i] == ' ':
+                k = 0
+            else:
+                k = ord(s[i]) - 64
+            total = total + (i + 1) * k
+        answer.append(total)
