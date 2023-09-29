396e36c85b0e2af84a1a106b58ea2111883f31b1
Jangchaeyun <zzangsally@naver.com>
2023-09-29 17:14:20 +0900

    0929

---
+++ 브론즈2PY/0929/B12517.py
@@ -0,0 +1,5 @@
+t = int(input())
+for i in range(1, t + 1):
+    text = input().strip()
+    print("Case #{}: ".format(i) + text + " is ruled by " +
+          ("nobody." if text[-1] == "y" else "a queen." if text[-1] in ["a", "e", "i", "o", "u"] else "a king."))
