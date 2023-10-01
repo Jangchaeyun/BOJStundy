590efadd2efdff0c7c1e620bd40ac65a247bffea
Jangchaeyun <zzangsally@naver.com>
2023-10-01 11:32:17 +0900

    1001

---
+++ 브론즈2PY/1001/B13627.py
@@ -0,0 +1,19 @@
+from sys import stdin
+
+
+def solution(N, R, rlist):
+    flag = 0
+    for i in range(1, N + 1):
+        if i in rlist:
+            continue
+        else:
+            print(i, end=' ')
+            flag = 1
+    if flag == 0:
+        print('*')
+
+
+N, R = map(int, stdin.readline().rstrip().split())
+rlist = list(map(int, stdin.readline().rstrip().split()))
+
+solution(N, R, rlist)
---
+++ 브론즈2PY/1001/B14175.py
@@ -0,0 +1,14 @@
+import sys
+
+M, N, K = map(int, sys.stdin.readline().split())
+
+li = []
+for i in range(M):
+    s = sys.stdin.readline().split()
+    li.append(s)
+
+for i in range(M):
+    for j in range(K):
+        for k in range(N):
+            print(li[i][0][k] * K, end="")
+        print()
