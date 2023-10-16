4b5f0914e361ff994776853b259580d2491b7e6c
Jangchaeyun <zzangsally@naver.com>
2023-10-16 12:37:45 +0900

    1016

---
+++ 브론즈1/Java/_1016/src/B1813.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B1813 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[51];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            int input = Integer.parseInt(st.nextToken());
+            arr[input]++;
+        }
+
+        for (int i = N; i >= 0; i--) {
+            if (arr[i] == i) {
+                System.out.println(i);
+                return;
+            }
+        }
+
+        System.out.println(-1);
+    }
+}
